aaaa17d3d3fad23353912ca4e0b9924abb6a73e7
LucasGutierreCraveiro <lucasgutierrecraveiro@outlook.com>
2020-11-26 12:17:45 -0300

    Adicionado módulo de animação

--- client/base.js
+++ client/base.js
@@ -1,3 +1,6 @@
+const { on } = require("../database/model/User");
+const Wait = require('../wait');
+
 (() => {
 
 	global.on(
@@ -29,3 +32,73 @@
 	)
 })()
 
+
+/**
+ * 
+ * Bloco da Animação
+ * 
+ * 
+ * @param {*} upper 
+ * @param {*} seq 
+ * @param {*} looping 
+ */
+
+exports.playAnimation = function(upper,seq,looping) {
+	let anims = {};
+	// let anim_ids = 
+	if (seq.task) {
+		//para animação aqui
+		stopAnimation(true);
+
+		let ped = PlayerPedId()
+		if (seq.task == 'PROP_HUMAN_SEAT_CHAIR_MP_PLAYER') {
+			const { x, y, z} = getPosition();
+			TaskStartScenarioAtPosition(ped,seq.task,x,y,z-1,GetEntityHeading(ped),0,0,false);
+		} else {
+			TaskStartScenarioInPlace(ped,seq.task,0, seq.play_exit)
+		}
+	} else {
+		stopAnimation(upper)
+
+		let flags = 0
+		if (upper) {
+			flags = flags + 48; //Entender melhor essas constantes aqui
+		}
+
+		if (looping) {
+			flags = flags + 1;
+		}
+
+		// (()=>{
+			
+		// }
+		// )();
+
+	}
+}
+
+exports.stopAnimation = function(upper) {
+	let anims = {}
+	if (upper) {
+		ClearPedSecondaryTask(PlayerPedId());
+	} else {
+		ClearPedTasks(PlayerPedId());
+	}
+}
+
+/**
+ *  Posicional
+ */
+
+exports.getPosition = function() {
+	const {x,y,z} = GetEntityCoords(PlayerPadId(),true);
+	return {x,y,z};
+}
+
+/**
+ * Weapons
+ */
+
+exports.clearWeapons = function() {
+	RemoveAllPedWeapons(PlayerPedId(),true);
+}
